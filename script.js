84ebab9e630640a88498f8ada92317ae6feebfb1
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-06-29 13:53:29 -0400

    added eye icons and popup

---
+++ script.js
@@ -0,0 +1,18 @@
+const addButton = document.getElementsByClassName(".profile-button-action");
+
+let myLibrary = [];
+
+class Book {
+  constructor(title, author, description, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.pages = pages;
+    this.read = read;
+  }
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
