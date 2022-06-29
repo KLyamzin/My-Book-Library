c214ed5d0177cac1c46fe2b96d8bca44eb260a15
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-06-29 15:41:38 -0400

    connected Add & Close buttons

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-const addButton = document.getElementsByClassName(".profile-button-action");
+const addButton = document.querySelectorAll("[data-add-button]");
+const closeButton = document.querySelectorAll("[data-close-button]");
+const overlay = document.getElementById("overlay");
+/* open and close buttons */
+addButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(button.dataset.addButton);
+    openModal(modal);
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+closeButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = document.querySelector(".modal-add-book");
+    closeModal(modal);
+  });
+});
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
 
 let myLibrary = [];
 
@@ -10,7 +38,6 @@ class Book {
     this.pages = pages;
     this.read = read;
   }
-  // the constructor...
 }
 
 function addBookToLibrary() {
