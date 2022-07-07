7b82f1412c401f9c2186e9a2447455d740f3b671
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-07-07 13:36:02 -0400

    duplicate check functionality is complete

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const overlay = document.getElementById("overlay");
 const cardContainer = document.querySelector(".card-container");
 const addBookForm = document.querySelector("#add-book-form");
 const addBookModal = document.querySelector("#modal-add-book");
-
+const errorTitle = document.getElementById("errorTitle");
 /* open and close buttons */
 addButton.forEach((button) => {
   button.addEventListener("click", () => {
@@ -32,12 +32,11 @@ overlay.addEventListener("click", () => {
   closeModal();
   // });
 });
-submitButton.addEventListener("submit", (e) => {
+/* submitButton.addEventListener("submit", (e) => {
   // e.preventDefault();
   // const modal = document.querySelector(".modal-add-book");
-  closeModal();
-  // addBookToLibrary();
-});
+  // closeModal();
+}); */
 /* function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove("active");
@@ -47,33 +46,10 @@ function closeModal() {
   // if (modal == null) return;
   addBookModal.classList.remove("active");
   overlay.classList.remove("active");
+  addBookForm.reset();
+  errorTitle.classList.remove("active");
 }
-
-/*  add a book to the library */
-/* let myLibrary = [
-  {
-    title: "The Hobbit",
-    author: "John Smith",
-    isbn: "123456",
-    description: "The Hobbits and the lord of the ring",
-    read: true,
-  },
-  {
-    title: "The Matrix",
-    author: "Wachovski",
-    isbn: "3394853",
-    description: "The Matrix Beginning",
-    read: true,
-  },
-  {
-    title: "Witcher",
-    author: "John Mickel",
-    isbn: "993456",
-    description: "Cool dude killing bad things",
-    read: false,
-  },
-]; */
-
+/* constructors */
 class Book {
   constructor(title, author, isbn, description, read) {
     this.title = title;
@@ -97,25 +73,56 @@ class Library {
     ];
   }
   addBook(newBook) {
-    this.books.push(newBook);
+    if (!this.duplicateCheck(newBook)) {
+      this.books.push(newBook);
+    }
   }
   removeBook(isbn) {
     this.books = this.books.filter((book) => book.isbn !== isbn);
   }
+  duplicateCheck(newBook) {
+    return this.books.some((book) => book.title === newBook.title);
+  }
 }
+
+/* add book to collection*/
 const library = new Library();
+
+function getInputValue() {
+  const title = document.querySelector("#book-title").value;
+  const author = document.querySelector("#author-name").value;
+  const description = document.querySelector("#description").value;
+  const isbn = document.querySelector("#isbn").value;
+  const read = document.querySelector("#read").checked;
+  return new Book(title, author, isbn, description, read);
+}
+
+function addBookToLibrary(e) {
+  e.preventDefault();
+  const newBook = getInputValue();
+  if (library.duplicateCheck(newBook)) {
+    errorTitle.classList.add("active");
+  } else {
+    library.addBook(newBook);
+    saveLocal();
+    displayBook();
+    closeModal();
+  }
+}
+
+addBookForm.onsubmit = addBookToLibrary;
 
 function displayBook() {
   resetBook();
-  // restoreLocal();
-  // saveLocal();
   for (let book of library.books) {
     createCard(book);
   }
 }
+
 function resetBook() {
   cardContainer.innerHTML = "";
 }
+
 function createCard(book) {
   const card = document.createElement("div");
   const title = document.createElement("h3");
@@ -149,24 +156,6 @@ function createCard(book) {
   cardContainer.appendChild(card);
 }
 
-function getInputValue() {
-  const title = document.querySelector("#book-title").value;
-  const author = document.querySelector("#author-name").value;
-  const description = document.querySelector("#description").value;
-  const isbn = document.querySelector("#isbn").value;
-  const read = document.querySelector("#read").checked;
-  return new Book(title, author, isbn, description, read);
-}
-function addBookToLibrary(e) {
-  e.preventDefault();
-  const newBook = getInputValue();
-  library.addBook(newBook);
-  saveLocal();
-  displayBook();
-  closeModal();
-}
-
-addBookForm.onsubmit = addBookToLibrary;
 displayBook();
 
 // save to local storage
@@ -175,7 +164,6 @@ function saveLocal() {
     localStorage.setItem("library", JSON.stringify(library.books));
   }
 }
-
 function restoreLocal() {
   const books = JSON.parse(localStorage.getItem("library"));
   if (books) {
